6da5157d91f6c581d57eec8166c6814c7802d624
Shaik Abdul Baji <shaik.baji.7393@gmail.com>
2023-08-03 22:27:33 +0530

    Full TestNG Course

---
+++ TestNG/src/test/java/b73UninstallingOfTestNG/aDemo.java
@@ -0,0 +1,7 @@
+package b73UninstallingOfTestNG;
+
+public class aDemo {
+	
+	// Eclipse IDE --> Help --> Install New Software --> Already Installed 
+
+}
---
+++ TestNG/src/test/java/b74TestNGRetryusingIRetryAnalyzerandIAnnotationTransformer/MyRetryAnalyzer.java
@@ -0,0 +1,24 @@
+package b74TestNGRetryusingIRetryAnalyzerandIAnnotationTransformer;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class MyRetryAnalyzer implements IRetryAnalyzer{
+		
+	int counter = 0;
+	int retryLimit = 3;
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		
+		if(counter<retryLimit)
+		{
+			
+			counter ++;
+			return true;
+		}
+		
+		return false;
+	}
+
+}
